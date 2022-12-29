0c7584929d546de07a035220f74d493671cd4eeb
Arun S. Sekher <arunshekher@users.noreply.github.com>
2022-12-29 12:11:53 +0400

    update how cron is printed to crontabs file

--- containers/cron/startup.sh
+++ containers/cron/startup.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+# echo
 echo "Starting startup.sh.."
-echo "*/3       *       *       *       *       php /var/www/e107/cron_test.php >> /var/log/cron/cron.log 2>&1" >> /etc/crontabs/root
+# echo "*/3       *       *       *       *       php /var/www/e107/cron_test.php >> /var/log/cron/cron.log 2>&1" >> /etc/crontabs/root
 
+cat <<EOT >> /etc/crontabs/root
+*/3       *       *       *       *       php /var/www/cron_test.php >> /var/log/cron/cron.log 2>&1
+*/5       *       *       *       *       php /var/www/e107/cron.php >> /var/log/cron/cron.log 2>&1
+line 2
+EOT
+
+# list
 crontab -l
 
+# run
 crond -f
