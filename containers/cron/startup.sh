51f396faad42dda2d98b0ab8bfdd286a92dff2f8
Arun S. Sekher <arunshekher@users.noreply.github.com>
2023-01-01 21:02:45 +0400

    add new cron script

--- containers/cron/startup.sh
+++ containers/cron/startup.sh
@@ -5,8 +5,9 @@ echo "Starting startup.sh.."
 # echo "*/3       *       *       *       *       php /var/www/e107/cron_test.php >> /var/log/cron/cron.log 2>&1" >> /etc/crontabs/root
 
 cat <<EOT >> /etc/crontabs/root
-*/3       *       *       *       *       php /var/www/scripts/cron-tests.php >> /var/log/cron/cron.log 2>&1
-*       *       *       *       *       php /var/www/e107/cron.php token=5ecdf6b535fc08ac99dcc7b03d6c554536a91a6f >/dev/null 2>&1
+*/5       *       *       *       *       php /var/www/scripts/php-mail-test.php >> /var/log/cron/cron.log 2>&1
+*/15       *       *       *       *       php /var/www/scripts/phpmailer-smtp-test.php >> /var/log/cron/cron.log 2>&1
+*       *       *       *       *       php /var/www/e107/cron.php token=5ecdf6b535fc08ac99dcc7b03d6c554536a91a6f >> /var/log/cron/cron.log 2>&1
 EOT
 
 # list
